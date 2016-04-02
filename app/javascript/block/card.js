a17b12932e9fef9efad39089bc6ec87967d74086
Marco Bruno <marco.bruno.br@gmail.com>
2016-04-02 01:21:21 -0300

    Exercício do capítulo 3: Pegando o a cor da bolinha pra mudar a cor do cartão.

--- app/javascript/block/card.js
+++ app/javascript/block/card.js
@@ -2,6 +2,7 @@ var $cardColors = document.querySelectorAll('.card-colors');
 
 for (var index = 0; index < $cardColors.length; index++) {
   $cardColors[index].addEventListener('click', function(event) {
-    console.log('Você clicou!!!');
+    var $card = this.parentNode.parentNode.parentNode;
+    $card.dataset.color = this.dataset.color;
   });
 };
