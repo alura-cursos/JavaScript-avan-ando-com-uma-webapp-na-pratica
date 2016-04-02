8bf56f326e4985ed3d2b05000a9bc553608cf585
Marco Bruno <marco.bruno.br@gmail.com>
2016-04-02 01:34:45 -0300

    Exercício do capítulo 3: Feedback de qual foi a última bolinha clicada.

--- app/javascript/block/card.js
+++ app/javascript/block/card.js
@@ -4,5 +4,11 @@ for (var index = 0; index < $cardColors.length; index++) {
   $cardColors[index].addEventListener('click', function(event) {
     var $card = this.parentNode.parentNode.parentNode;
     $card.dataset.color = this.dataset.color;
+
+    for (var position = 0; position < $cardColors.length; position++) {
+      $cardColors[position].classList.remove('isActive');
+    };
+
+    this.classList.add('isActive');
   });
 };
