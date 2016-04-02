d5309166c2e83dee10e58e40ca99950c6544b419
Marco Bruno <marco.bruno.br@gmail.com>
2016-04-02 01:41:32 -0300

    Exercício do capítulo 3: Menos loop sempre é feliz.

--- app/javascript/block/card.js
+++ app/javascript/block/card.js
@@ -1,14 +1,19 @@
+var $cards = document.querySelectorAll('.card');
 var $cardColors = document.querySelectorAll('.card-colors');
 
-for (var index = 0; index < $cardColors.length; index++) {
-  $cardColors[index].addEventListener('click', function(event) {
-    var $card = this.parentNode.parentNode.parentNode;
-    $card.dataset.color = this.dataset.color;
+for (var index = 0; index < $cards.length; index++) {
+  $cards[index].addEventListener('click', function (event) {
+    var $this = event.target;
+    var $card = this;
 
-    for (var position = 0; position < $cardColors.length; position++) {
-      $cardColors[position].classList.remove('isActive');
+    if ($this.classList.contains('card-colors')) {
+      $card.dataset.color = $this.dataset.color;
+
+      for (var position = 0; position < $cardColors.length; position++) {
+        $cardColors[position].classList.remove('isActive');
+      };
+
+      $this.classList.add('isActive');
     };
-
-    this.classList.add('isActive');
   });
 };
